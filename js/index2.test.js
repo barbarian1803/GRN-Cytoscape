875b5c8bb4923ccec75a8b4bd17b58303b013762
Bharata Kalbuaji <bharata1803@gmail.com>
2017-11-01 19:25:16 +0900

    save loaded data

--- js/index2.test.js
+++ js/index2.test.js
@@ -1,14 +1,17 @@
 var network = null;
+var network_file_raw = null;
+var position_file_raw = null;
 
 $("#testclick").on("click",function(e){
 	var network_file = "http://localhost/WebNetwork/data/combine_network_filtered.csv";
 	$.get(network_file,function(data){
+		network_file_raw = data;
 		network = buildNetwork(data);
 
 		var pos_xy = "http://localhost/WebNetwork/data/node_pos.csv";
 		$.get(pos_xy,function(data){
+			position_file_raw = data;
 			parsePosFile(data);
-			console.log(network)
 		});
 	});
 });
